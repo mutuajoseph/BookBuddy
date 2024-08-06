a4589d34757cff1cbfd9ac61a61e6113e40ff4b0
mutuajoseph <wambuajoseph756@gmail.com>
2024-08-06 09:48:31 +0300

    Added logic to load data to the ui

--- src/components/booklist/BookList.jsx
+++ src/components/booklist/BookList.jsx
@@ -1,16 +1,48 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner, Text } from "@chakra-ui/react";
 import { booksData } from "../../constants";
 import { BookItem } from "../bookitem/BookItem";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export const BookList = () => {
-  const [books, setBooks] = useState(booksData);
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // load the books
+    // we are suppposed to update the state with the loaded books
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setBooks(booksData);
+      setLoading(false);
+    }, 10000);
+  };
 
   return (
     <Box p={"2rem"}>
-        {books.map((book, index) => (
-            <BookItem key={index} book={book}/>
-        ))}
+      {loading ? (
+        <>
+          <Spinner />
+        </>
+      ) : (
+        <>
+          {books.length > 0 ? (
+            <>
+              {books.map((book, index) => (
+                <BookItem key={index} book={book} />
+              ))}
+            </>
+          ) : (
+            <>
+              <Text>No books to show </Text>
+            </>
+          )}
+        </>
+      )}
     </Box>
   );
 };
